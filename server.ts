dbb2846c60d9819c704bbe4780b9c8715e431198
gtn <gtn@mac-gtn.local>
2026-07-03 02:15:23 -0400

    DONE: Resources available | TODO: Models and others entities

--- server.ts
+++ server.ts
@@ -1,10 +1,19 @@
 import express from 'express';
-import {testConnection} from './src/config/connection.ts';
+import routes from './src/index.js';
+import { logError, handleError } from './src/middlewares/errorHandler.js';
+import { testConnection } from './src/config/connection.js';
 
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+app.use(express.json());
+app.use(routes);
+
+// Manejo de errores (siempre despues de las rutas)
+app.use(logError);
+app.use(handleError);
+
 await testConnection();
 
-app.listen(PORT, () => console.log(`running on port:${PORT}`))
+app.listen(PORT, () => console.log(`running on port:${PORT}`));
